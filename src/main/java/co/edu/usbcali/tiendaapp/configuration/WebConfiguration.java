f6df72f2d2fad96ac1da16df0a09443070404b7e
Raul Hernandez <raulandres462@gmail.com>
2023-06-25 14:22:52 -0500

    Se revisó sonar

--- src/main/java/co/edu/usbcali/tiendaapp/configuration/WebConfiguration.java
+++ src/main/java/co/edu/usbcali/tiendaapp/configuration/WebConfiguration.java
@@ -1,7 +1,5 @@
 package co.edu.usbcali.tiendaapp.configuration;
 
-import jakarta.validation.constraints.NotNull;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
